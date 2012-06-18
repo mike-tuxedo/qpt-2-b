f4bcccd97f2f000ea228b39f20b2a2cb7ecb63f0
Daniel Krenmayr <dkrenmayr.mmt-b2010@fh-salzburg.ac.at>
2012-06-18 22:58:44 +0200

    refactoring, adding barcode to autocomplete

--- autocomplete.php
+++ autocomplete.php
@@ -1,10 +1,15 @@
 <?php
+
 include 'header.php';
 include 'configParameter.php';
 
 $requestCharacter = '%'.$_POST['queryString'].'%';
 
-$sql = "SELECT id, nme FROM article WHERE articleStatusID = '".$articleStatus."' AND nme LIKE ? LIMIT 0, 10";
+$sql = "SELECT id, nme 
+		FROM article 
+		WHERE articleStatusID = '".$articleStatus."' 
+			AND (nme LIKE ? OR barcode LIKE ?) 
+		LIMIT 0, 10";
 
 $stmt = $connect -> prepare($sql);
 $stmt -> bind_param('s', $requestCharacter);
@@ -12,11 +17,10 @@ $stmt -> execute();
 $stmt -> bind_result($articleID, $articleName);
 
 while($stmt -> fetch())
-{
     echo '<li onClick="selectValue(\''.utf8_encode($articleName).'\');">
 		  <a href="productDetail.php?aid='.$articleID.'" title="'.utf8_encode($articleName).'">
 		  '.utf8_encode($articleName).'</li>';
-}
 
 $stmt -> close();
+
 ?>
--- productDetail.php
+++ productDetail.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'header.php';
 include 'configParameter.php';
 include 'checkGETParams.php';
@@ -6,17 +7,14 @@ include 'checkGETParams.php';
 $articleID = $_GET['aid'];
 
 // valuations of this product
-$sql = "SELECT
-			vlAnimal.valuationclassid, vlAnimal.nme, vlAnimal.description,
-			vlEco.valuationclassid, vlEco.nme, vlEco.description,
+$sql = "SELECT 
+			vlAnimal.valuationclassid, vlAnimal.nme, vlAnimal.description, 
+			vlEco.valuationclassid, vlEco.nme, vlEco.description, 
 			vlSocial.valuationclassid, vlSocial.nme, vlSocial.description
-
 		FROM articleValuation av
-
-		LEFT JOIN valuationclass_language vlAnimal ON av.animalprotection_vclassid = vlAnimal.valuationclassid
-		LEFT JOIN valuationclass_language vlEco ON av.ecologic_vclassid = vlEco.valuationclassid
-		LEFT JOIN valuationclass_language vlSocial ON av.social_vclassid = vlSocial.valuationclassid
-
+			LEFT JOIN valuationclass_language vlAnimal ON av.animalprotection_vclassid = vlAnimal.valuationclassid
+			LEFT JOIN valuationclass_language vlEco ON av.ecologic_vclassid = vlEco.valuationclassid
+			LEFT JOIN valuationclass_language vlSocial ON av.social_vclassid = vlSocial.valuationclassid
 		WHERE av.articleid = ?";
 
 $stmt = $connect -> prepare($sql);
@@ -26,37 +24,21 @@ $stmt -> bind_result($valuationID['animal'], $valuationName['animal'], $valuatio
 $stmt -> fetch();
 $stmt -> close();
 
-/*
-optional
-
-while ($row = $result->fetch_object()) {
-	$row->host;
-}
-
------
-
-query: 	"company" on table c means the name of the merchant company
-		"company" on table cl means the name of the manufacturer
-*/
-
-$sql = "SELECT
-			a.nme, a.brand, a.pricefrom, a.substancedeclaredtxt,
-			ast.gpSort,
-			c.nme, c.infoGMO, cl.nme, cl.note_public,
+$sql = "SELECT 
+			a.nme, a.brand, a.pricefrom, a.substancedeclaredtxt, 
+			ast.gpSort, 
+			c.nme, c.infoGMO, cl.nme, cl.note_public,  
 			t.nme, t.countrycode
-
-		FROM
-			articleStatus ast,
-			company c,
-			territory t,
+		FROM 
+			articleStatus ast, 
+			company c, 
+			territory t, 
 			article a
-
 		LEFT JOIN company cl ON a.producerid = cl.id
-
-		WHERE
-			a.id = ?
-			AND a.articleStatusID = ast.statusID
-			AND a.salescompanyid = c.id
+		WHERE 
+			a.id = ? 
+			AND a.articleStatusID = ast.statusID 
+			AND a.salescompanyid = c.id 
 			AND a.territoryid = t.id";
 
 $stmt = $connect -> prepare($sql);
@@ -66,82 +48,96 @@ $stmt -> bind_result($articleName, $articleBrand, $articlePrice, $articleSubstan
 $stmt -> fetch();
 $stmt -> close();
 
-$content .= '<div id="productDetail">
-				<p class="productTitle">'.utf8_encode($articleName).'</p>
-				<div id="productDetailHeader">
-					<span>
-						<a id="addProduct" href="addRemoveProduct.php?product='.$articleName.'&action=add"><img src="images/addProduct.png" alt="zur Einkaufliste"/></a>
-						<p>Marke: '.utf8_encode($brand).'</p>
-						<p>Preis: '.$articlePrice.'</p>
-						<p>territory: '.$territoryName.', '.$territoryCountryCode.'</p>
-						<p>';
-
-// calculate valuation of each class
-foreach($valuationID as $key => $value)
-{
-	$content .='<p></p>
-				<div class="ratingContainer">'.$valuationNameCategory[$key].'</div>
-				<div class="ratings">';
-
-	for($i = 0; $i < 3; $i++)
-	{
-		if($value < 0)
-		{
-			$content .= '<img src="images/valuationRatingCircles/noRating.png" />';
-		}
-		else
-		{
-			$content .= '<img src="images/valuationRatingCircles/'.$value.'.png" />';
-		}
-	}
-
-	$content .= '</div>';
-}
-
-$content .= '</span>
-		</div>
-	</div>
-	<div id="productDetailNavigation">
-		<a href="#">Bewertungen</a>
-		<span>';
-
-foreach($valuationName as $key => $value)
-{
-	$content .= '<p class="valuationCategory">'.ucfirst($key).'</p>';
-
-	if($valuationID[$key] > 0)
-		$content .= $valuationNameArray[utf8_encode($value)].'<p class="valuationDescription">'.utf8_encode($valuationDescription[$key]).'</p>';
-	else
-		$content .= '<p class="valuationNotFound">keine Bewertung</p>';
-}
-
-$content .= '</span>';
-
-if(!empty($manufacturerName))
-	$content .= '
-		<a href="#">Hersteller</a>
+$content .= '
+<div id="productDetail">
+	
+	<p class="productTitle">'.utf8_encode($articleName).'</p>
+	
+	<div id="productDetailHeader">
 		<span>
-			<p class="valuationCategory">'.utf8_encode($manufacturerName).'</p>
-			<p class="toCenter">'.utf8_encode($manufacturerNote).'</p>
-		</span>';
+			<a id="addProduct" href="addRemoveProduct.php?product='.$articleName.'&action=add" id="addToBasket"><img src="images/addProduct.png" alt="zur Einkaufliste"/></a>
+			<p>Marke: '.utf8_encode($brand).'</p>
+			<p>Preis: '.$articlePrice.'</p>
+			<p>territory: '.$territoryName.', '.$territoryCountryCode.'</p>
+			<p>';
+			
+			// calculate valuation of each class
+			foreach($valuationID as $key => $value)
+			{
+				$content .= '
+					<p></p>
+					<div class="ratingContainer">'.$valuationNameCategory[$key].'</div>
+					<div class="ratings">';
+				
+				for($i = 0; $i < 3; $i++)
+					if($value < 0)
+						$content .= '<img src="images/valuationRatingCircles/noRating.png" />';
+					else 
+						$content .= '<img src="images/valuationRatingCircles/'.$value.'.png" />';	
+				
+				$content .= '</div>';
+			}
 
-if(!empty($articleSubstance))
-	$content .= '
-		<a href="#">Inhaltsstoffe</a>
-		<span>
-			<p class="toCenter">'.utf8_encode($articleSubstance).'</p>
-		</span>';
-
-if(!empty($articleSubstance))
-	$content .= '
-		<a href="#">Vertreiber</a>
-		<span>
-			<p class="valuationCategory">'.utf8_encode($companyName).'</p>
-			<p class="toCenter"><strong>InfoGMO</strong><br /> '.utf8_encode($companyInfoGMO).'</p>
+$content .= '
 		</span>
-		';
-
+	</div>
+</div>
+<div id="productDetailNavigation">
+	<a href="#">Bewertungen</a>
+	<span>';
+	
+	foreach($valuationName as $key => $value)
+	{
+		$content .= '<p class="valuationCategory">'.ucfirst($key).'</p>';
+		
+		if($valuationID[$key] > 0)
+			$content .= $valuationNameArray[utf8_encode($value)].
+						'<p class="valuationDescription">'.utf8_encode($valuationDescription[$key]).'</p>';
+		else 
+			$content .= '<p class="valuationNotFound">keine Bewertung</p>';			
+	}
+	
+	$content .= '</span>';
+	
+	if(!empty($manufacturerName))
+	{
+		$content .= '
+			<a href="#">Hersteller</a>
+			<span>
+				<p class="valuationCategory">'.utf8_encode($manufacturerName).'</p>';
+		
+		if(!empty($manufacturerNote))		
+			$content .= '<p class="toCenter">'.utf8_encode($manufacturerNote).'</p>';
+		else	
+			$content .= '<p class="toCenter">Es sind uns keine weiteren Informationen zu diesem Hersteller bekannt.</p>';
+			
+		$content .= '</span>';	
+	}
+	
+	if(!empty($articleSubstance))
+		$content .= '
+			<a href="#">Inhaltsstoffe</a>
+			<span>
+				<p class="toCenter">'.utf8_encode($articleSubstance).'</p>
+			</span>';	
+	
+	if(!empty($articleSubstance))
+	{
+		$content .= '
+			<a href="#">Vertreiber</a>
+			<span>
+				<p class="valuationCategory">'.utf8_encode($companyName).'</p>';
+				
+		if(!empty($companyInfoGMO))	
+			$content .= '<p class="toCenter"><strong>InfoGMO</strong><br /> '.utf8_encode($companyInfoGMO).'</p>';
+		else 
+			$content .= '<p class="toCenter">Es sind uns keine weiteren Informationen zu diesem Vertreiber bekannt.</p>';
+			
+		$content .= '</span>';
+	}	
+	
 $content .= '</div>	';
 
 include 'footer.php';
+
 ?>
